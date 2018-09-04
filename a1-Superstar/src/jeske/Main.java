f19c0d234e79a5f68cefd4964f9338317f72c244
Nico Jeske <nico.jeske@gmail.com>
2018-09-04 20:34:58 +0200

    Initial Commit

---
+++ a1-Superstar/src/jeske/Main.java
@@ -0,0 +1,10 @@
+package jeske;
+
+import java.io.BufferedReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(Main.class.getResourceAsStream(""))
+    }
+}
